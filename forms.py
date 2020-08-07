db8f98e765bf13ac245f85686d34cb15b2a183ba
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-07 12:01:39 -0700

    added passwordfield and updated titles for inputs on form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, PasswordField
 from wtforms.validators import InputRequired, Optional, NumberRange, URL, AnyOf, Length, Email
 
 
@@ -15,10 +15,10 @@ class AddUserForm(FlaskForm):
     """Form for registering new users."""
     
     username = StringField("Username", validators=[Length(min=0, max=20, message="Username can't be more than 30 characters.")])
-    password = StringField("Species of Pet")
-    email = StringField("Username", validators=[Length(min=0, max=50, message="An email can't be more than 50 characters.")])
-    first_name = StringField("Username", validators=[Length(min=0, max=30, message="A first name can't be more than 30 characters.")])
-    last_name = StringField("Username", validators=[Length(min=0, max=30, message="A last name can't be more than 30 characters.")])
+    password = PasswordField("Password")
+    email = StringField("Email", validators=[Length(min=0, max=50, message="An email can't be more than 50 characters.")])
+    first_name = StringField("First Name", validators=[Length(min=0, max=30, message="A first name can't be more than 30 characters.")])
+    last_name = StringField("Last Name", validators=[Length(min=0, max=30, message="A last name can't be more than 30 characters.")])
     
 
 
